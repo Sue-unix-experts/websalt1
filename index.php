432a75456128a138205aacbaa1c7e594522fcb03
Tom Scholten <tom.scholten@gmail.com>
2018-04-08 14:21:04 +0200

    Test branch contents

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>DrySalt TEST Website - not for production</title>
+    <link href="style.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <script src="jquery.min.js"></script>
+    <!-- parallax.js from https://github.com/pixelcog/parallax.js/ -->
+    <script src="parallax.min.js"></script>
+  </head>
+  <body>
+    <section>
+      <div class="container">
+        <h1>DrySalt non-caramel TEST git-branch</h1>
+        <h3>running on <?php echo $_SERVER['SERVER_NAME']; ?>/<?php echo $_SERVER['SERVER_ADDR']; ?></h1>
+      </div>
+    </section>
+
+    <!-- picture from pixabay -->
+    <div class="parallax-container" data-parallax="scroll" data-position="top" data-bleed="10" data-speed="0.5" data-image-src="america-651863_1920.jpg" data-natural-width="1920" data-natural-height="1080"></div>
+
+
+    <section>
+      <div class="container">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac sodales tortor. Donec libero felis, auctor sit amet erat eu, sagittis molestie magna. Sed venenatis mi at ante facilisis, facilisis dictum arcu efficitur. Nullam aliquam enim at nibh efficitur, eu rhoncus ligula laoreet. Curabitur eu congue sem, in congue eros. Duis ornare tortor quis consectetur pretium. Mauris sed arcu id quam rhoncus pellentesque. Vestibulum eleifend eros libero.</p>
+
+        <p>Vestibulum vel enim felis. Pellentesque viverra mollis felis in tempor. Etiam sagittis mattis blandit. Mauris a est ornare, auctor mauris at, fringilla felis. Proin quis viverra nulla. Donec venenatis elementum sollicitudin. Nunc pulvinar blandit dolor, sit amet interdum libero facilisis vitae.</p>
+
+        <p>Nullam nec felis iaculis est pellentesque elementum. Maecenas dapibus aliquet velit, quis consequat ante euismod id. Fusce justo arcu, ultrices vel metus id, suscipit laoreet eros. Donec blandit ex sapien, at eleifend justo ornare id. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi tempus turpis neque, vitae laoreet enim mollis in. Duis euismod justo sit amet nibh dictum venenatis. Morbi faucibus, odio eget fermentum faucibus, mi enim tristique orci, sit amet elementum lectus lectus eget elit. Praesent euismod sed purus at ornare. Ut at justo mi.</p>
+      </div>
+    </section>
+
+    <!-- picture from pixabay -->
+    <div class="parallax-container" data-parallax="scroll" data-bleed="10" data-speed="0.2" data-image-src="salt-lake-594606_1920.jpg" data-natural-width="1920" data-natural-height="1321"></div>
+
+    <section>
+      <div class="container">
+        <p>Mauris in mauris in magna laoreet venenatis. Curabitur aliquam sagittis leo, a dapibus neque dignissim at. Integer a ipsum ipsum. Donec rhoncus accumsan felis, nec aliquam nibh cursus eu. Sed malesuada tempor dapibus. Fusce suscipit, dolor ut suscipit ultricies, purus ex egestas nulla, non fermentum velit libero sit amet orci. Aenean convallis neque et nunc tristique, nec consectetur lectus eleifend. Suspendisse potenti. Sed nunc ex, facilisis id volutpat id, tincidunt ac justo. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+
+        <p>Nulla scelerisque nec purus vitae elementum. Ut facilisis condimentum urna, ac volutpat lorem tincidunt eget. Quisque mattis commodo ipsum et ullamcorper. Donec odio metus, hendrerit at metus vitae, lobortis dapibus ipsum. Nullam gravida lorem est, at imperdiet eros rhoncus vitae. Sed scelerisque faucibus est, ut pellentesque nisi congue ac. Integer ac mauris varius, molestie ex sed, condimentum odio. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+      </div>
+    </section>
+
+
+    <footer>
+      <div class="container">
+	Copyright &copy; 2018 by WebSalt Inc. - the imaginary company for our <a href="https://sue2018.snow.nl">SUE 2018</a> workshop 
+      </div>
+    </footer>
+  </body>
+</html>
